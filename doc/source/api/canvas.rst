aab6ae19ebcd018f4128a74f1a9c04edc9862f06
Michael Brown <mbrown1413@gmail.com>
2012-10-07 09:20:45 -0400

    Some canvas documentation

--- doc/source/api/canvas.rst
+++ doc/source/api/canvas.rst
@@ -1,5 +1,30 @@
 
-Canvas
+Canvas Interface
 =====================================
+
+A canvas provides a place for the rendered output to go.  Output can be written
+to a file, written in memory, displayed to the user, etc.
+
+Implementations
+---------------
+
+.. todo:: Canvas Implementations
+
+Workflow
+--------
+
+:c:func:`Canvas_start` and :c:func:`Canvas_finish` are called at the beginning
+and end of interacting with the canvas.  :c:func:`Canvas_get_next_row` and
+:c:func:`Canvas_write_row` are called for every row in order from top to bottom
+to provide data to the canvas.
+
+.. todo:: Boilerplate example
+
+The implementation can assume that it is used correctly.  For example there
+don't need to be any checks to make sure :c:func:`Canvas_get_next_row` is
+called before :c:func:`Canvas_start`.
+
+Reference
+---------
 
 .. autocfile:: canvas.h
--- include/psyrender/canvas.h
+++ include/psyrender/canvas.h
@@ -1,8 +1,39 @@
 /**
- * A canvas contains the properties of an image such as width and height, and
- * handles writing the image to some medium such as an image file.
  *
- * .. todo:: Canvas Interface
+ * .. c:type:: Canvas
+ *
+ *     .. c:member:: unsigned int width
+ *     .. c:member:: unsigned int height
+ *
+ * .. c:function:: bool Canvas_start(Canvas* canvas)
+ *
+ *     Prepares the canvas to be used by resetting or initializing the state.
+ *
+ *     Returns true on success, false on failure.
+ *
+ * .. c:function:: Color* Canvas_get_next_row(Canvas* canvas)
+ *
+ *     Returns a row for the user to write to.  The first call returns the top
+ *     row, followed by the second row, etc.  The row doesn't necessarily have
+ *     to be different every time it is called, depending on how the canvas is
+ *     implemented.
+ *
+ *     When the user is finished writing to this row,
+ *     :c:func:`Canvas_write_row` should be called to write the row to the
+ *     canvas.
+ *
+ * .. c:function:: bool Canvas_write_row(Canvas* canvas, Color* row)
+ *
+ *     .. todo:: Canvas_write_row
+ *
+ * .. c:function:: bool Canvas_finish(Canvas* canvas)
+ *
+ *     .. todo:: Canvas_finish
+ *
+ * .. c:function:: void Canvas_free(Canvas* canvas)
+ *
+ *     .. todo:: Canvas_free
+ *
  */
 
 #ifndef _PSY_CANVAS_H
